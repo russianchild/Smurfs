d9cf5fcc4b521af2aabd2f2da7ad749044f6e2f3
russianchild <144118223+russianchild@users.noreply.github.com>
2025-08-20 21:45:47 +0530

    Update AITherapistChat.tsx

--- src/components/AITherapistChat.tsx
+++ src/components/AITherapistChat.tsx
@@ -1,8 +1,8 @@
 const AITherapistChat = () => {
   return (
     <div className="min-h-screen flex items-center justify-center p-4">
-      <elevenlabs-convai agent-id="agent_3101k30sa82nedpvjrbzbmf83m6q"></elevenlabs-convai>
-      <script src="https://unpkg.com/@elevenlabs/convai-widget-embed" async type="text/javascript"></script>
+      <elevenlabs-convai agent-id="agent_01jztfbc2jf708phwmsfxww2xh"></elevenlabs-convai>
+        <script src="https://unpkg.com/@elevenlabs/convai-widget-embed" async type="text/javascript"></script>
     </div>
   );
 };
